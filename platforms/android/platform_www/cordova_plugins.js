cc407d18e8c69359bead76b8a104d21b965838b7
Daniel Zhang <jmzwcn@gmail.com>
2018-10-27 20:23:32 +0800

    build.
    Signed-off-by: Daniel Zhang <jmzwcn@gmail.com>

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -31,13 +31,6 @@ module.exports = [
 ];
 module.exports.metadata = 
 // TOP OF METADATA
-{
-    "cordova-plugin-device": "2.0.2",
-    "cordova-plugin-ionic-keyboard": "2.1.3",
-    "cordova-plugin-ionic-webview": "1.2.1",
-    "cordova-plugin-splashscreen": "5.0.2",
-    "cordova-plugin-statusbar": "2.4.2",
-    "cordova-plugin-whitelist": "1.3.3"
-};
+{}
 // BOTTOM OF METADATA
 });
