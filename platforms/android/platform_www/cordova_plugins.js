c46895d6bf060b10c62fe981be001a4c18bb1176
Daniel Zhang <jmzwcn@gmail.com>
2018-10-27 20:09:09 +0800

    build.
    Signed-off-by: Daniel Zhang <jmzwcn@gmail.com>

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -34,6 +34,7 @@ module.exports.metadata =
 {
     "cordova-plugin-device": "2.0.2",
     "cordova-plugin-ionic-keyboard": "2.1.3",
+    "cordova-plugin-ionic-webview": "1.2.1",
     "cordova-plugin-splashscreen": "5.0.2",
     "cordova-plugin-statusbar": "2.4.2",
     "cordova-plugin-whitelist": "1.3.3"
