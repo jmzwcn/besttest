9ebd0cec2ac61afdc2a86e7da057d7e2be909b67
Daniel Zhang <jmzwcn@gmail.com>
2018-10-28 18:41:18 +0800

    build.
    Signed-off-by: Daniel Zhang <jmzwcn@gmail.com>

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -31,6 +31,14 @@ module.exports = [
     "clobbers": [
       "window.StatusBar"
     ]
+  },
+  {
+    "id": "cordova-plugin-ionic-webview.IonicWebView",
+    "file": "plugins/cordova-plugin-ionic-webview/src/www/util.js",
+    "pluginId": "cordova-plugin-ionic-webview",
+    "clobbers": [
+      "Ionic.WebView"
+    ]
   }
 ];
 module.exports.metadata = 
@@ -38,10 +46,10 @@ module.exports.metadata =
 {
   "cordova-plugin-device": "2.0.2",
   "cordova-plugin-ionic-keyboard": "2.1.3",
-  "cordova-plugin-ionic-webview": "1.2.1",
   "cordova-plugin-splashscreen": "5.0.2",
   "cordova-plugin-statusbar": "2.4.2",
-  "cordova-plugin-whitelist": "1.3.3"
+  "cordova-plugin-whitelist": "1.3.3",
+  "cordova-plugin-ionic-webview": "2.2.0"
 };
 // BOTTOM OF METADATA
 });
